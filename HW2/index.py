f47ab7447667dc711689e2ac470b5dea8e404022
Simon Larspers Qvist <simonqv@kth.se>
2023-02-14 17:54:14 +0800

    index almost done?

--- HW2/index.py
+++ HW2/index.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-import re
-import nltk
 import sys
 import getopt
 import os
-import bisect
 import pickle
-from collections import Counter
+# nltk.download('punkt')
+# nltk.download('stopwords')
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
 
 def usage():
     print("usage: " + sys.argv[0] + " -i directory-of-documents -d dictionary-file -p postings-file")
+
 
 def build_index(in_dir, out_dict, out_postings):
     """
@@ -21,34 +24,48 @@ def build_index(in_dir, out_dict, out_postings):
     # Pls implement your code in below
     # nltk.download()
     path = "./nltk_data/corpora/reuters/first"
+    # path = "./nltk_data/corpora/reuters/" + in_dir
 
     # Should this be a list?
     inv_ind = {}
-    with os.scandir(path) as it:
-        # TODO: Optimize with BSBI or SPIMI
-        # TODO: remove numbers
-        for entry in it:
-            file = open(entry, 'r')
-            file_name = int(entry.name)
-            lines = file.readlines()
-            for line in lines:
-                for word in line.split():   # https://stackoverflow.com/questions/21023901/how-to-split-at-all-special-characters-with-re-split
-                    word = word.strip(" .,+:").lower()
-                    if word not in inv_ind:
-                        inv_ind[word] = []
-                    bisect.insort(inv_ind[word], file_name)
-    term_doc_freq = []
-    for k in sorted(inv_ind.keys()):
-        term_doc_freq.append((k, len(set(inv_ind[k]))))
-
+    dictionary = {}
+    stemmer = PorterStemmer()
+    stop_words = set(stopwords.words('english'))
     postings_lists = {}
-    for k in inv_ind:
-        postings_lists[k] = sorted(set(inv_ind[k]))
+
+    try:
+        with os.scandir(path) as it:
+            for entry in it:
+                file = open(entry, 'r')
+                file_name = int(entry.name)
+                lines = file.readlines()
+                for line in lines:
+                    tokens = [word_tokenize(t) for t in sent_tokenize(line)]
+                    if len(tokens) != 0:
+                        for t in tokens[0]:
+                            stemmed = stemmer.stem(t).lower()
+                            if stemmed in dictionary and stemmed not in stop_words:
+                                postings_lists[stemmed].append(file_name)
+                            elif stemmed not in dictionary and stemmed not in stop_words:
+                                dictionary[stemmed] = 0
+                                postings_lists[stemmed] = [file_name]
+
+    except IOError:
+        print("No such file in path:", path)
+
+    term_doc_freq = []
+    for k in sorted(postings_lists.keys()):
+        term_doc_freq.append((k, len(set(postings_lists[k]))))
+
+    for k in postings_lists:
+        postings_lists[k] = sorted(set(postings_lists[k]))
 
     with open("dictionary-file", "wb") as handle:
         pickle.dump(term_doc_freq, handle, protocol=pickle.HIGHEST_PROTOCOL)
     with open("postings-file", "wb") as handle:
         pickle.dump(postings_lists, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    print("indexing done...")
 
 
 input_directory = output_file_dictionary = output_file_postings = None
@@ -60,11 +77,11 @@ except getopt.GetoptError:
     sys.exit(2)
 
 for o, a in opts:
-    if o == '-i': # input directory
+    if o == '-i':  # input directory
         input_directory = a
-    elif o == '-d': # dictionary file
+    elif o == '-d':  # dictionary file
         output_file_dictionary = a
-    elif o == '-p': # postings file
+    elif o == '-p':  # postings file
         output_file_postings = a
     else:
         assert False, "unhandled option"
